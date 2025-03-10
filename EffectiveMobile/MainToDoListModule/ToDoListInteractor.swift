8db0efe1a03de60187d88136eac98055e838eba6
IrinaMuravyova <miomir@yandex.ru>
2025-03-10 19:10:18 +0800

    The basic structure of VIPER with the interconnection of layers for the main module has been prepared.

---
+++ EffectiveMobile/MainToDoListModule/ToDoListInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoListInteractor.swift
+//  EffectiveMobile
+//
+//  Created by Irina Muravyeva on 10.03.2025.
+//
+
+import Foundation
---
+++ EffectiveMobile/MainToDoListModule/ToDoListModuleConfigurator.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoListModuleConfigurator.swift
+//  EffectiveMobile
+//
+//  Created by Irina Muravyeva on 10.03.2025.
+//
+
+import Foundation
